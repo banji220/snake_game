3a10b39f6286e4dd0333f2db866cc6a44838fb48
Banji <khanjani1997@gmail.com>
2020-08-11 15:24:21 +0430

    Changed the pen.goto(0, 260) to pen.goto(0, 360), so the position of the scores are fine now!

--- snake_game.py
+++ snake_game.py
@@ -37,7 +37,7 @@ pen.shape("square")
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 260)
+pen.goto(0, 360)
 pen.write("Score: 0    High Score: 0", align="center", font = ("Courier", 24, "normal"))
 # Go Functions
 def go_up():
