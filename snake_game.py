9348898cd514073684e3809fdc8ab3fa51ff83dd
Banji <khanjani1997@gmail.com>
2020-08-11 15:22:29 +0430

    Adding Score and High Score at the top of the windows

--- snake_game.py
+++ snake_game.py
@@ -30,8 +30,15 @@ food.goto(0, 100)
 # Segments List
 segments = []
 
-
-
+# Pen 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0    High Score: 0", align="center", font = ("Courier", 24, "normal"))
 # Go Functions
 def go_up():
     if head.direction != "Down":
