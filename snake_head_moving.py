f16d0c885b475cce3ed97f987b81fb7a880244dc
Banji <khanjani1997@gmail.com>
2020-08-13 15:34:54 +0430

    Completed second part of the tutorial, binding go function with keyboard.

--- snake_head_moving.py
+++ snake_head_moving.py
@@ -15,8 +15,8 @@ head.speed(0)
 head.shape("square")
 head.color("black")
 head.penup()
-# head.goto(0, 0)
-head.direction = "Up"
+head.goto(0, 0)
+head.direction = "stop"
 
 
 
@@ -53,6 +53,13 @@ def move():
     if head.direction == "Right":
         x = head.xcor()
         head.setx(x + 10)
+        
+# Keyboard Binding
+windows.listen()
+windows.onkeypress(go_up, key = "w")
+windows.onkeypress(go_down, key = "s")
+windows.onkeypress(go_left, key = "a")
+windows.onkeypress(go_right, key = "d")
 # Main Game Loop
 while True:
     windows.update()
